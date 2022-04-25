934ccdea975918753600a14baebec698764be23d
goelayu <goelayu@umich.edu>
2022-04-25 07:21:58 -0400

    add apache dev to deps

--- bash/mm-inst.sh
+++ bash/mm-inst.sh
@@ -1,4 +1,4 @@
 
 # installation dependendencies for mahimahi 
-sudo apt-get install protobuf-compiler  libprotobuf-dev autotools-dev dh-autoreconf iptables pkg-config dnsmasq-base apache2-bin debhelper libssl-dev ssl-cert libxcb-present-dev libcairo2-dev libpango1.0-dev  libboost-all-dev
+sudo apt-get install protobuf-compiler apache2-dev libprotobuf-dev autotools-dev dh-autoreconf iptables pkg-config dnsmasq-base apache2-bin debhelper libssl-dev ssl-cert libxcb-present-dev libcairo2-dev libpango1.0-dev  libboost-all-dev
 
