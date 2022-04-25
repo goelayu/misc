79f568ee79afba8af815aa74b5dbc6135bcd20e8
goelayu <goelayu@umich.edu>
2022-04-25 07:14:46 -0400

    add mm deps

---
+++ bash/mm-inst.sh
@@ -0,0 +1,4 @@
+
+# installation dependendencies for mahimahi 
+sudo apt-get install protobuf-compiler  libprotobuf-dev autotools-dev dh-autoreconf iptables pkg-config dnsmasq-base apache2-bin debhelper libssl-dev ssl-cert libxcb-present-dev libcairo2-dev libpango1.0-dev  libboost-all-dev
+
