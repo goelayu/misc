2a5cc7a1a4dd896be9f5344308ddda6aa820f2c2
goelayu <goelayu@umich.edu>
2022-10-27 10:01:57 -0400

    add bash template containing good practices

---
+++ bash/template.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# A template of good bash practices learnt over the
+# years of bash scripting
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./script.sh arg-one arg-two
+
+This is an awesome bash script to make your life better.
+
+'
+    exit
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+    echo do awesome stuff
+}
+
+main "$@"
+
